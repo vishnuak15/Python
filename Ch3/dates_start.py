023e92f87a9ed7414a0ba1725fc869c347ef4c0d
Vishnu <vishnuak15@gmail.com>
2021-10-28 21:22:52 +0530

    commit done

--- Ch3/dates_start.py
+++ Ch3/dates_start.py
@@ -1,25 +1,29 @@
 #
 # Example file for working with date information
 #
-
+from datetime import date,time,datetime
 
 def main():
   ## DATE OBJECTS
   # Get today's date from the simple today() method from the date class
-
-
+  today = date.today()
+  print("Todays date is ",today)
+  print("Today's date is ",today.day,today.month,today.year)
   # print out the date's individual components
-
-  
+  print("Todays week day",today.weekday())
+  days=['monday','tuesday','wednsday','thursday','friday','saturday','sunday']
+  print("Todays date is:",days[today.weekday()])
   # retrieve today's weekday (0=Monday, 6=Sunday)
 
   
   ## DATETIME OBJECTS
   # Get today's date from the datetime class
-
+  today = datetime.now()
+  print("The current datetime is:",today)
   
   # Get the current time
-
+  t=datetime.time(datetime.now())
+  print(t)
  
 
   
